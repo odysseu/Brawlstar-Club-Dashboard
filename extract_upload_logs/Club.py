ab84d98cc474f95154eaa285a30c671dae011f07
qz5mrh <ulysse.boucherie@insee.fr>
2022-05-17 20:55:32 +0000

    Supressed GCD related files

---
+++ extract_upload_logs/Club.py
@@ -0,0 +1,9 @@
+class Club():
+    def __init__(self, clubTag, clubName, description, requiredTrophies, trophies):
+        self.clubTag = clubTag
+        self.clubName = clubname
+        self.decription = description
+        self.requiredTrophies = requiredTrophies
+        self.trophies = trophies
+
+
