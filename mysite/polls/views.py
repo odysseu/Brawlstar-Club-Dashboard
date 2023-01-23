e4c82b08711ecd312d0de06a40df1431dee53fff
Ulysse Boucherie <ulysse.boucherie@insee.fr>
2023-01-23 23:40:01 +0100

    arrived at part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
+    html = "<html><h1> Local Time: {{ TIME_ZONE }} </h1><head><style> body{background-color: #000000; color: #ffffff;} </style></head><body>This is in dark mode</body></html>"
+    return HttpResponse(html)
